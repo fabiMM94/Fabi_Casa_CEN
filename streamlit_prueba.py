e2e7b485eb651ef9b7196b57cf3c4927e3471cb7
fabiMM94 <fabian.medina@coordinador.cl>
2025-03-17 16:08:52 -0300

    stream lit prueba 2
    sdaasd

--- streamlit_prueba.py
+++ streamlit_prueba.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title("¡Hola, Streamlit!")
 st.write("Esta es mi primera aplicación web con Streamlit.")
+
+
+if st.button("Presiona para cambiar el estado"):
+    st.write("¡Me hiciste clic! 😃")
+else:
+    st.write("Aún no has presionado el botón. 😴")
